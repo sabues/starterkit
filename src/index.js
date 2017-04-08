f56502b5c6aa441a4d0b0e686837ab5962a19291
dseveso <daniel.seveso@auth0.com>
2017-04-08 18:38:57 -0500

    add linting with ESLint

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-
-debugger;
+//debugger;
 
 // backticks are part of the ES6 display template features
-console.log(`I would pay ${courseValue} for this awesome course!`);
+console.log(`I would pay ${courseValue} for this awesome course!`); 
