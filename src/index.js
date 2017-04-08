889a65b9ef6b319f78b78adceb4d078c332659cb
dseveso <daniel.seveso@auth0.com>
2017-04-08 17:34:21 -0500

    add bundling ES6 and webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+debugger;
+
+// backticks are part of the ES6 display template features
+console.log(`I would pay ${courseValue} for this awesome course!`);
